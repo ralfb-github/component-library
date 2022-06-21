d32d800acf71da0423864abd3a081671b0b3f534
Ralf Braitling <ralf.braitling@cluno.com>
2022-06-21 15:31:07 +0200

    fix lint

--- src/fields/Field.spec.tsx
+++ src/fields/Field.spec.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Field } from './Field';
-jest.mock('../utils/useUniqueID');
+jest.mock('../utils/useUniqueId');
 describe('Input', () => {
   it('applies unique ID to id attribute', () => {
     render(
